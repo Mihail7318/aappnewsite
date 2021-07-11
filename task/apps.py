906f421a1cf6a43af3bd0501a9c11328e98cd2f1
zaydulin <zaydulin.vagiz@gmail.com>
2021-07-11 22:19:13 +0400

    commit

--- task/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class TaskConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'task'
-    verbose_name = 'Урок'
-    verbose_name_plural = 'Уроки'
