faf98f50ae636972f003860c1ab7fc101a95244a
zaydulin <zaydulin.vagiz@gmail.com>
2021-07-17 17:03:48 +0400

    commit

--- taski/models.py
+++ taski/models.py
@@ -1,4 +1,5 @@
 from news.models import *
+from django.contrib.auth.models import User
 
 from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
@@ -20,6 +21,7 @@ class Zadacha(models.Model):
     rubric = models.ForeignKey('news.Category', on_delete=models.CASCADE, verbose_name='Рубрика')
     image = models.ImageField(blank=True, upload_to='media/image/', null=True, verbose_name='Изображение')
     views = models.IntegerField(default=0, verbose_name='Количество просмотров')
+    time = models.TimeField(default=0, verbose_name='Количество просмотров')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     update_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
 
@@ -39,15 +41,20 @@ class Zadacha(models.Model):
 
 class Vopros(models.Model):
 
+    author = models.ForeignKey(User, default='self', related_name='poster', on_delete=models.CASCADE)
     title = models.CharField(max_length=255, db_index=True, verbose_name='Наименование')
     content = RichTextUploadingField(blank=True, verbose_name='Описание')
     image = models.ImageField(blank=True, upload_to='media/image/', null=True, verbose_name='Изображение')
+    image_1 = models.ImageField(blank=True, upload_to='media/image/', null=True, verbose_name='Изображение')
     varian_1 = models.CharField(max_length=255, db_index=True, verbose_name='Ответ №1')
     otvet_1 = models.BooleanField(default=False, verbose_name='Верный')
+    image_2 = models.ImageField(blank=True, upload_to='media/image/', null=True, verbose_name='Изображение')
     varian_2 = models.CharField(max_length=255, db_index=True, verbose_name='Ответ №2')
     otvet_2 = models.BooleanField(default=False, verbose_name='Верный')
+    image_3 = models.ImageField(blank=True, upload_to='media/image/', null=True, verbose_name='Изображение')
     varian_3 = models.CharField(max_length=255, db_index=True, verbose_name='Ответ №3')
     otvet_3 = models.BooleanField(default=False, verbose_name='Верный')
+    image_4 = models.ImageField(blank=True, upload_to='media/image/', null=True, verbose_name='Изображение')
     varian_4 = models.CharField(max_length=255, db_index=True, verbose_name='Ответ №4')
     otvet_4 = models.BooleanField(default=False, verbose_name='Верный')
     views = models.IntegerField(default=0, verbose_name='Количество пройденых')
